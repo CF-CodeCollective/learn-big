0be095e7211a256618c949594096886c4cf6e9c7
PetersonE1 <PetersonE_1@live.com>
2023-10-30 18:46:52 -0400

    set up commenting on first sample code snippet

--- Sample_Code/hello_world.py
+++ Sample_Code/hello_world.py
@@ -1,6 +1,10 @@
+# Set up these variables to display your own name on your branch!
 first_name = "First"
 last_name = "Last"
 
+# This is done for a specific reason: any python script can be imported as a module, and all the code will
+# be run from the top down. This lets you write setup code, but anything under this 'if' statement will only
+# run if this file is launched directly, not imported.
 if __name__ == "__main__":
     print("Hello, World!")
     print("Hello,", first_name, last_name)
