134234c5b06728a9bb6de95c810360110827abbc
muneebsocute <79283232+muneebsocute@users.noreply.github.com>
2023-11-14 17:38:01 -0500

    Update hello_world.py

--- Sample_Code/hello_world.py
+++ Sample_Code/hello_world.py
@@ -1,6 +1,6 @@
 # Set up these variables to display your own name on your branch!
-first_name = "First"
-last_name = "Last"
+first_name = "Muneeb"
+last_name = "Elyaman"
 
 # This is done for a specific reason: any python script can be imported as a module, and all the code will
 # be run from the top down. This lets you write setup code, but anything under this 'if' statement will only
