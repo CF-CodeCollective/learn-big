8936804be4f8755d02fdba1ee637b020eae5a755
Gavin <145506874+WallerG27@users.noreply.github.com>
2023-11-14 17:26:53 -0500

    Name changed for "hello_world.py"
    Added name.

--- Sample_Code/hello_world.py
+++ Sample_Code/hello_world.py
@@ -1,8 +1,9 @@
 # Set up these variables to display your own name on your branch!
-first_name = "First"
-last_name = "Last"
+#This is a comment.
+first_name = "Gavin"
+last_name = "Waller"
 
-# This is done for a specific reason: any python script can be imported as a module, and all the code will
+# This is done for a specific reason: any Python script can be imported as a module, and all the code will
 # be run from the top down. This lets you write setup code, but anything under this 'if' statement will only
 # run if this file is launched directly, not imported.
 if __name__ == "__main__":
