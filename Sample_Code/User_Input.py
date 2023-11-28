6032b8216fdab4102e718951551eade58ec36b3c
PetersonE1 <PetersonE_1@live.com>
2023-11-28 17:09:04 -0500

    Add files via upload

---
+++ Sample_Code/User_Input.py
@@ -0,0 +1,16 @@
+# Step 1: Get user input
+x = input("Enter prompt here: ")
+
+# Step 2: Validate user input
+try:
+    x = int(x)
+except:
+    x = input("Retry prompt: ")
+
+# Step 3: Put the validation in a loop
+while True:
+    try:
+        pass
+        break
+    except:
+        pass
